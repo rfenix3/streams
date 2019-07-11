5567cd0e31f771eed0cde2530720b8ea42fcb315
Richard Fenix <rfenix@Richards-MBP.attlocal.net>
2019-07-10 22:19:48 -0400

    Render authentication status.

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,20 +1,37 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
   componentDidMount(){
-    window.gapi.load('client:auth2', ()=> {
-      window.gapi.client.init({
-        clientId: '929596800669-uglib6d73pmtsma7vnc9r6tul51smkim.apps.googleusercontent.com',
-        scope: 'email'
-      });
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client
+        .init({
+          clientId:
+            '929596800669-uglib6d73pmtsma7vnc9r6tul51smkim.apps.googleusercontent.com',
+          scope: 'email'
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get()});
+        });
     });
   }
 
+  renderAuthButton() {
+    console.log(this.state.isSignedIn);
+    if (this.state.isSignedIn === null) {
+      return <div>I do not know if we are signed in.</div>
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in.</div>
+    } else {
+      return <div>I am NOT signed in.</div>
+    }
+  }
+
   render() {
-    return <div>Google Auth</div>
+    return <div>{this.renderAuthButton()}</div>
   }
 }
-
-// THis note is a Test if changes are detected by git...
 
 export default GoogleAuth;
